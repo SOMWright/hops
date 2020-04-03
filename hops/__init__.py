e43329bc495b7a5dc44110a6de75622c9f90afe7
Angelos Tsiaras <aggelostsiaras@gmail.com>
2020-04-03 18:15:49 +0100

    test

--- hops/__init__.py
+++ hops/__init__.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '2.6.1'
-__message__ = 'Important - Circular aperture photometry applied\nFix - Fitting button disabled'
+__version__ = '2.7.0'
+__message__ = ''
 
 from .__run__ import run_app
 
